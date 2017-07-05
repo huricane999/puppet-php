0f8cd61b1ead159f86498419a55836ba33a363c9
Chris Smith <casmit14@ncsu.edu>
2017-07-05 13:15:05 -0400

    limit fact to linux (for now)

--- lib/facter/composerversion.rb
+++ lib/facter/composerversion.rb
@@ -1,21 +1,29 @@
 Facter.add(:composerversion_installed) do
   setcode do
-    output = Facter::Util::Resolution.exec('composer -V')
+    kernel = Facter.value(:kernel)
+    case kernel
+    when 'Linux'
+      output = Facter::Util::Resolution.exec('composer -V')
 
-    unless output.nil?
-      output.split("\n").first.split(' ').
-        select { |x| x =~ %r{^(?:(\d+)\.)(?:(\d+)\.)?(\*|\d+)} }.first
+      unless output.nil?
+        output.split("\n").first.split(' ').
+          select { |x| x =~ %r{^(?:(\d+)\.)(?:(\d+)\.)?(\*|\d+)} }.first
+      end
     end
   end
 end
 
 Facter.add(:composerversion_latest) do
   setcode do
-    raw = Facter::Util::Resolution.exec('curl -s \'https://api.github.com/repos/composer/composer/releases/latest\'')
+    kernel = Facter.value(:kernel)
+    case kernel
+    when 'Linux'
+      raw = Facter::Util::Resolution.exec('curl -s \'https://api.github.com/repos/composer/composer/releases/latest\'')
 
-    unless raw.nil?
-      parsed = JSON.parse(raw)
-      parsed['name']
+      unless raw.nil?
+        parsed = JSON.parse(raw)
+        parsed['name']
+      end
     end
   end
 end
