de683778495da27c48dadc55e29514b1de025f4d
Chris Smith <casmit14@ncsu.edu>
2017-07-05 12:00:01 -0400

    add fact for composer versions

---
+++ lib/facter/composerversion.rb
@@ -0,0 +1,21 @@
+Facter.add(:composerversion) do
+  setcode do
+    output = Facter::Util::Resolution.exec('composer -V')
+
+    unless output.nil?
+      output.split("\n").first.split(' ').
+        select { |x| x =~ %r{^(?:(\d+)\.)(?:(\d+)\.)?(\*|\d+)} }.first
+    end
+  end
+end
+
+Facter.add(:composerlatest) do
+  setcode do
+    raw = Facter::Util::Resolution.exec('curl -s \'https://api.github.com/repos/composer/composer/releases/latest\'')
+
+    unless raw.nil?
+      parsed = JSON.parse(raw)
+      parsed['name']
+    end
+  end
+end
